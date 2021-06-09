44f37007e4a2943eaf78ac270dc2233c32be878a
Franco Rosi <fraanrosi@gmail.com>
2021-06-09 02:25:07 -0300

    First commit. App working locally.

---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using Commander.Dtos;
+using Commander.Models;
+
+namespace Commander.Profiles
+{
+    public class CommanderProfile : Profile 
+    {
+        public CommanderProfile()
+        {
+            //Source -> Target
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<CommandUpdateDto, Command>();
+            CreateMap<Command, CommandUpdateDto>();
+        } 
+
+    }
+}
